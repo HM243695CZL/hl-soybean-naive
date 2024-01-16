5e735bbd74df05b8bf0eed273715bd69cfd0a215
HM243695CZL <1224311695@qq.com>
2024-01-16 11:29:54 +0800

    添加玻璃特效

---
+++ src/views/special-effects/component/normal-effects/glass-button.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="glass-button">
+    <button>glass-button</button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.glass-button {
+  button {
+    width: 200px;
+    background: rgba(57, 88, 64, 0.4);
+    color: white;
+    transition: 0.4s;
+    text-align: center;
+    padding: 10px;
+    border-radius: 4px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+    &:hover {
+      background: rgba(12, 80, 38, 0.67);
+      &::before {
+        transform: translateX(320px);
+        transition: 1s;
+        opacity: 0.7;
+      }
+      &::after {
+        transform: translateX(320px);
+        transition: 1s;
+        opacity: 1;
+      }
+    }
+    &::before,
+    &::after {
+      content: '';
+      display: block;
+      width: 80px;
+      height: 100%;
+      background: rgba(179, 255, 210, 0.5);
+      opacity: 0.5;
+      position: absolute;
+      top: 0;
+      left: 0;
+      filter: blur(30px);
+      transform: translateX(-100px);
+    }
+    &::after {
+      width: 40px;
+      background: rgba(179, 255, 210, 0.5);
+      left: 60px;
+      opacity: 0;
+      filter: blur(5px);
+    }
+  }
+}
+</style>
--- src/views/special-effects/normal-effects/index.vue
+++ src/views/special-effects/normal-effects/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { markRaw, reactive } from 'vue';
 import IconHoverShine from '@/views/special-effects/component/normal-effects/icon-hover-shine.vue';
+import GlassButton from '@/views/special-effects/component/normal-effects/glass-button.vue';
 interface SpecialItem {
   text: string;
   compKey: string;
@@ -10,12 +11,13 @@ const state = reactive({
   specialList: [
     {
       text: '图标hover发光',
-      compKey: 'IconHoverShine'
+      compKey: markRaw(IconHoverShine)
+    },
+    {
+      text: '玻璃按钮',
+      compKey: markRaw(GlassButton)
     }
-  ] as Array<SpecialItem>,
-  componentMap: {
-    IconHoverShine: markRaw(IconHoverShine)
-  }
+  ] as Array<SpecialItem>
 });
 </script>
 
@@ -24,8 +26,8 @@ const state = reactive({
     <div class="comp-list">
       <div v-for="item in state.specialList" :key="item.text" class="comp-item">
         <div class="name fw700 font-size-28px mb2">{{ item.text }}</div>
-        <div class="comp-box">
-          <component :is="state.componentMap[item.compKey]" />
+        <div class="comp-box mb4">
+          <component :is="item.compKey" />
         </div>
       </div>
     </div>
