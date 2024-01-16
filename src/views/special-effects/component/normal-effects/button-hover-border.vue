fe33d1b8f6c446171c859dfaa50e8cacb590148b
HM243695CZL <1224311695@qq.com>
2024-01-16 13:29:32 +0800

    添加“按钮hover边框”

---
+++ src/views/special-effects/component/normal-effects/button-hover-border.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="bg-#2c3a47 p4">
+    <div class="button-box">
+      <svg width="100%" height="100%">
+        <rect class="rectangle" width="100%" height="100%"></rect>
+      </svg>
+      <div class="btn">button-hover-border</div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.button-box {
+  width: 240px;
+  height: 60px;
+  text-align: center;
+  &:hover {
+    .rectangle {
+      animation: 0.5s extend linear forwards;
+    }
+  }
+  .rectangle {
+    stroke-width: 8px;
+    stroke: #25ccf7;
+    fill: transparent;
+    stroke-dasharray: 100 500;
+    stroke-dashoffset: -372;
+  }
+  .btn {
+    margin-top: -40px;
+    color: #fff;
+  }
+}
+@keyframes extend {
+  to {
+    stroke-dasharray: 600;
+    stroke-dashoffset: 0;
+    stroke-width: 2;
+  }
+}
+</style>
--- src/views/special-effects/normal-effects/index.vue
+++ src/views/special-effects/normal-effects/index.vue
@@ -3,6 +3,7 @@ import { markRaw, reactive } from 'vue';
 import IconHoverShine from '@/views/special-effects/component/normal-effects/icon-hover-shine.vue';
 import GlassButton from '@/views/special-effects/component/normal-effects/glass-button.vue';
 import SvgTextDrawing from '@/views/special-effects/component/normal-effects/svg-text-drawing.vue';
+import ButtonHoverBorder from '@/views/special-effects/component/normal-effects/button-hover-border.vue';
 interface SpecialItem {
   text: string;
   compKey: string;
@@ -21,6 +22,10 @@ const state = reactive({
     {
       text: 'svg文字绘制',
       compKey: markRaw(SvgTextDrawing)
+    },
+    {
+      text: '按钮hover边框',
+      compKey: markRaw(ButtonHoverBorder)
     }
   ] as Array<SpecialItem>
 });
@@ -39,4 +44,17 @@ const state = reactive({
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.name {
+  position: relative;
+  &::before {
+    content: '';
+    width: 4px;
+    height: 20px;
+    background: #126ac6;
+    position: absolute;
+    top: 12px;
+    left: -10px;
+  }
+}
+</style>
